f4b7d3e51868f20b89c8f1504ff88975378564d5
aidfromdeagland <aidfromdeagland@gmail.com>
2020-10-07 12:34:21 +0300

    feat:add basic styling and layout

---
+++ src/app/note-card/note-card.component.ts
@@ -0,0 +1,29 @@
+import {AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-note-card',
+  templateUrl: './note-card.component.html',
+  styleUrls: ['./note-card.component.scss']
+})
+export class NoteCardComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('truncator') truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText') bodyText: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    const bodyTextStyle = window.getComputedStyle(this.bodyText.nativeElement);
+    const viewAbleHeight = parseInt(bodyTextStyle.getPropertyValue('height'), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewAbleHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
+}
